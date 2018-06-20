ae135b98a4589c3c37b1d9b4f6a39b1a0c5f029e
jtx007 <jamesjacobthomas7@gmail.com>
2018-06-20 17:46:21 -0400

    update game logic

---
+++ lib/draft.rb
@@ -0,0 +1,61 @@
+require 'pry'
+q1 = "What's your favorite season?"
+ans = ["(1)  Winter", "(2)  Spring", "(3)  Summer", "(4)  Fall"]
+
+q2 = "What's your favorite color scheme?"
+ans2 = ["(1) ocean", "(2) sunset", "(3) rainbow" ]
+
+q3 = "Marvel or DC?"
+ans3 = ["(1) marvel", "(2) dc"]
+
+q4 = "Pick a superpower:"
+ans4 = ["(1) invisibility", "(2) flight", "(3) invulnerability"]
+
+q5 = "What color is the dress?"
+ans5 = ["(1) blue and black", "(2) white and gold"]
+
+q6 = "How do you eat cereal?"
+ans6 = ["(1) milk first", "(2) cereal first"]
+
+q7 = "Are you..."
+ans7 = ["(1) left-handed", "(2) right-handed"]
+
+q8 = "Are you a...?"
+ans8 = ["(1) cat-person", "(2) dog-person", "(3) no pets"]
+
+q9 = "Is the earth round or flat?"
+ans9 = ["(1) definitely flat", "(2) definitely round", "(3) definitely boxy"]
+
+q10 = "Is a hot dog a sandwich?"
+ans10 = ["(1) yes, of course", "(2) no, it's a hotdog"]
+
+# puts ans
+
+
+
+
+
+
+
+
+# def question_and_answer(question, ans)
+#   puts question
+#   puts ans
+#   user_input = gets.chomp
+#   while !(1..ans.length).include? (user_input.to_i)
+#    puts "INVALID INPUT. Please try again: "
+#    user_input =  gets.chomp
+#   end
+#
+# end
+#
+# question_and_answer(question_1, ans)
+# question_and_answer(question_2, ans2)
+# question_and_answer(question_3, ans3)
+# question_and_answer(question_4, ans4)
+# question_and_answer(question_5, ans5)
+# question_and_answer(question_6, ans6)
+# question_and_answer(question_7, ans7)
+# question_and_answer(question_8, ans8)
+# question_and_answer(question_9, ans9)
+# question_and_answer(question_10, ans10)
